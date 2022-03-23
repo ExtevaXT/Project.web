58e915d3818b30fd7d08cf943afa9679267a4683
XT <alexmaintest99@gmail.com>
2022-03-23 15:25:52 +0500

    Layout V1.0.0 - Fixes, added awesome user panel (need some fix)

--- js/panel.js
+++ js/panel.js
@@ -1,5 +1,34 @@
-jQuery('ul.nav > li').hover(function() {
-    jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
-}, function() {
-    jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
-})
+document.addEventListener("DOMContentLoaded", function(){
+// make it as accordion for smaller screens
+    if (window.innerWidth > 992) {
+
+        document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
+
+            everyitem.addEventListener('mouseover', function(e){
+
+                let el_link = this.querySelector('a[data-bs-toggle]');
+
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.add('show');
+                    nextEl.classList.add('show');
+                }
+
+            });
+            everyitem.addEventListener('mouseleave', function(e){
+                let el_link = this.querySelector('a[data-bs-toggle]');
+
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.remove('show');
+                    nextEl.classList.remove('show');
+                }
+
+
+            })
+        });
+
+    }
+// end if innerWidth
+});
+// DOMContentLoaded  end
